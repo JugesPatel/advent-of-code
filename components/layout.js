a7c7179d3efa863bfef6f19a8aa39ab7e0428373
Jugal Patel <jpate43@uwo.ca>
2022-09-20 15:16:31 -0400

    Unoptimized images

--- components/layout.js
+++ components/layout.js
@@ -29,6 +29,7 @@ export default function Layout({ children, home }) {
               height={144}
               width={144}
               alt={name}
+              unoptimized
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
@@ -43,6 +44,7 @@ export default function Layout({ children, home }) {
                   height={108}
                   width={108}
                   alt={name}
+                  unoptimized
                 />
               </a>
             </Link>
