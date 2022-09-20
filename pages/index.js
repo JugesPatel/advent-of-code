ce47f23e0fbc7cfa6ded5174b80a06cda330096b
Jugal Patel <jpate43@uwo.ca>
2022-09-20 15:26:55 -0400

    Fix imports

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import DayOne from './posts/dayOneInteractive';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
